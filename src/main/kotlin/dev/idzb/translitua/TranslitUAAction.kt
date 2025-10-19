b1d7d2493c7e7e7d6fde5c4793a83814a9979dd0
Ihor Dziuba <ego.dzb@icloud.com>
2025-10-19 15:18:09 +0100

    Init

---
+++ src/main/kotlin/dev/idzb/translitua/TranslitUAAction.kt
@@ -0,0 +1,10 @@
+package dev.idzb.translitua
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class TranslitUAAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        println("Hello")
+    }
+}
